5a825b930750cc57c927d9102816e82a004c4b6c
Israel Duarte <israeltduarte@gmail.com>
2023-08-19 14:21:03 -0300

    update add role endpoints

--- myinvoice-back/src/main/java/br/com/isertech/myinvoiceback/controller/RoleController.java
+++ myinvoice-back/src/main/java/br/com/isertech/myinvoiceback/controller/RoleController.java
@@ -1,4 +1,39 @@
 package br.com.isertech.myinvoiceback.controller;
 
+import br.com.isertech.myinvoiceback.dto.RoleDTO;
+import br.com.isertech.myinvoiceback.entity.Role;
+import br.com.isertech.myinvoiceback.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/roles")
 public class RoleController {
+
+    @Autowired
+    RoleService roleService;
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @GetMapping
+    public ResponseEntity<List<Role>> getAllRoles() {
+
+        List<Role> roles = roleService.getAllRoles();
+
+        return ResponseEntity.status(HttpStatus.OK).body(roles);
+    }
+
+    @PostMapping
+    public ResponseEntity<Role> registerRole(@RequestBody RoleDTO dto) {
+
+        Role role = roleService.registerRole(dto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(role);
+    }
+
 }
